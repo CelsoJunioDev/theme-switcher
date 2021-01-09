27eaf271d81cb4997a710848697936c11a0fe43e
CelsoJunioDev <celsoj18@gmail.com>
2021-01-09 17:15:04 -0300

    starting the theme-light

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Header from '../src/components/Header'
 import GlobalStyle from './styles/global'
 
+import { ThemeProvider } from 'styled-components'
+import light from './styles/themes/light'
+
 function App() {
   return (
+    <ThemeProvider theme={light}>
+
     <div className="App">
       <GlobalStyle/>
       <Header/>
     </div>
+    </ThemeProvider>
   );
 }
 

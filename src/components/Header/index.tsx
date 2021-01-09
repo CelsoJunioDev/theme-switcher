28ff434ce275cb7bbe108f09d89576b1377bd919
CelsoJunioDev <celsoj18@gmail.com>
2021-01-09 17:53:06 -0300

    switch added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import Switch from 'react-switch'
+import {ThemeContext} from 'styled-components'
+
 import {Container} from './styles'
 
 const Header: React.FC = () =>{
+    const { colors } = useContext(ThemeContext)
     return (
 
         <Container>
             Hello World
+            <Switch
+            onChange={() => {}}
+            checked={false}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            width={40}
+            height={10}
+            handleDiameter={20}
+            offColor=""
+            onColor={colors.secundary}
+            />
         </Container>
     )
 }
